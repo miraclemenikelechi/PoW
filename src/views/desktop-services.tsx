806cf9dd254c1b0940218239cbd0e7915f827991
miraclemenikelechi <miracle.emenike7@gmail.com>
2026-01-17 21:09:26 +0100

    refactor: comment out desktop and mobile view components, disable services view

--- src/views/desktop-services.tsx
+++ src/views/desktop-services.tsx
@@ -1,12 +1,12 @@
-import { Fragment } from "react";
+// import { Fragment } from "react";
 
-import Services from "@/components/service";
+// import Services from "@/components/service";
 
-export default function Component() {
-    return (
-        <Fragment>
-            <Services type="frontend" />
-            <Services type="backend" />
-        </Fragment>
-    );
-}
+// export default function Component() {
+//     return (
+//         <Fragment>
+//             <Services type="frontend" />
+//             <Services type="backend" />
+//         </Fragment>
+//     );
+// }
