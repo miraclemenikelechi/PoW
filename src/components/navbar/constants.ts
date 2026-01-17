7ff0e7b5b22b25616a3b96bf041e0a08b13b7e31
miraclemenikelechi <miracle.emenike7@gmail.com>
2026-01-17 23:38:20 +0100

    feat: add responsive skills view, smooth scroll, and update navigation links

--- src/components/navbar/constants.ts
+++ src/components/navbar/constants.ts
@@ -1,5 +1,5 @@
 export const NAVIGATION_LINKS: iLink[] = [
-    { href: "/skills", title: "skills" },
     { href: "/projects", title: "projects" },
     { href: "/contact", title: "contact" },
+    { href: "/blog", title: "blog" },
 ];
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,20 +2,31 @@ import "./index.scss";
 
 import { createFileRoute } from "@tanstack/react-router";
 
+import { AboutText } from "@/apps/landing/about-text";
+import { ProfilePic } from "@/apps/landing/profile-pic";
+import { SkillsDesktopView } from "@/apps/skills/desktop";
+import { useBreakpoint } from "@/hooks/use-breakpoint";
+import { useSmoothScroll } from "@/hooks/use-smooth-scroll";
 import { cn } from "@/lib/utils";
-
-import { AboutText } from "../apps/landing/about-text";
-import { ProfilePic } from "../apps/landing/profile-pic";
+import { Suspense } from "react";
 
 export const Route = createFileRoute("/")({
     component: RouteComponent,
 });
 
 function RouteComponent() {
+    useSmoothScroll({ enabled: useBreakpoint({ bp: "tablet", type: "min" }) });
+
     return (
-        <main className={cn("mx-auto grid max-w-7xl grid-rows-2", "lg:grid-cols-2", "home__wrapper")}>
-            <ProfilePic />
-            <AboutText />
-        </main>
+        <Suspense fallback={<></>}>
+            {useBreakpoint({ bp: "tablet", type: "max" }) ? (
+                <main className={cn("mx-auto grid max-w-7xl grid-rows-2", "lg:grid-cols-2", "home__wrapper")}>
+                    <ProfilePic />
+                    <AboutText />
+                </main>
+            ) : (
+                <SkillsDesktopView />
+            )}
+        </Suspense>
     );
 }
